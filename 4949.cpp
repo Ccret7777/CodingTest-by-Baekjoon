4ee4122096baa5275e40d3bbadf5969b6d70ba81
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:24:13 +0900

    Stack을 이용한 괄호맞추기

---
+++ 4949.cpp
@@ -0,0 +1,48 @@
+﻿using namespace std;
+#include <iostream>
+#include <stack>
+#include <string>
+
+bool IsBalanced(const string& str)
+{
+    stack<char> s;
+
+    for (char c : str)
+    {
+        if (c == '[' || c == '(')
+            s.push(c);
+
+        else if (c == ']')
+        {
+            if (s.empty() || s.top() != '[') return false;  
+            s.pop();
+        }
+
+        else if (c == ')')
+        {
+            if (s.empty() || s.top() != '(') return false; 
+            s.pop();
+        }
+    }
+
+    return s.empty();
+}
+
+int main()
+{
+    string line;
+
+    while (true)
+    {
+        getline(cin, line);
+
+        if (line == ".") break;
+
+        if (IsBalanced(line))
+        {
+            cout << "yes" << '\n';
+        }
+
+        else cout << "no" << '\n';
+    }
+}
