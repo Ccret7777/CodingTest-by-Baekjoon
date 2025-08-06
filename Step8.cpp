3e6c4aea261f6c73481e2b33e639d878aeaba9b3
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:02:13 +0900

    B진법 숫자 10진수로 변환

---
+++ Step8.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    string N;
+    int B;
+    cin >> N >> B;
+
+    int result = 0;
+    int power = 1;
+
+    for (int i = N.length() - 1; i >= 0; i--)
+    {
+        char c = N[i];
+        int value;
+
+        if ('0' <= c && c <= '9')
+            value = c - '0';
+        else
+            value = c - 'A' + 10;
+
+        result += value * power;
+        power *= B;
+    }
+
+    cout << result << '\n';
+
+    return 0;
+}
