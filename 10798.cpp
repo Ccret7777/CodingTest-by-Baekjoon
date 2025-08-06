6d2699e475a22d65fb53c54b42b7787d76e201e3
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:00:09 +0900

    2차원 배열 세로읽기/행렬 열따라 읽기

---
+++ 10798.cpp
@@ -0,0 +1,26 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    string sentence[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> sentence[i];
+    }
+
+    for (int j = 0; j < 15; j++)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (j < sentence[i].length())
+            {
+                cout << sentence[i][j];
+            }
+        }
+    }
+
+    return 0;
+}
