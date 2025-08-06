92a6dd087d15a765da48fc1ca9e982ddd72f6ce3
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:57:55 +0900

    크로아티아 알파벳/replace&find 함수 이용

---
+++ 2941.cpp
@@ -0,0 +1,25 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    string word;
+    cin >> word;
+
+    string croatia[] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+    for (string c : croatia)
+    {
+        size_t pos = word.find(c);   // size_t: 양수만 표현 unsigned int
+        while (pos != string::npos)
+        {
+            word.replace(pos, c.length(), "#");
+            pos = word.find(c);
+        }
+    }
+
+    cout << word.length() << '\n';
+
+    return 0;
+}
