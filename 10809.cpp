40184c55e7ba79dc9c31831dd1d0396913717b42
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:50:06 +0900

    단어에 알파벳이 몇 번째에 있는지 출력

---
+++ 10809.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int alphabet[26];
+    for (int i = 0; i < 26; i++) 
+    {
+        alphabet[i] = -1; 
+    }
+
+    for (int i = 0; i < s.length(); i++) 
+    {
+        int idx = s[i] - 'a';  
+        if (alphabet[idx] == -1) 
+        {
+            alphabet[idx] = i;  
+        }
+    }
+
+    for (int i = 0; i < 26; i++) 
+    {
+        cout << alphabet[i] << ' ';
+    }
+
+    cout << '\n';
+    return 0;
+}
