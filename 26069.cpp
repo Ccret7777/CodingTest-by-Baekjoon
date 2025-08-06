efe2aa465ea5a86aab98dcd96c077fe45d192d29
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:27:56 +0900

    count함수/unordered_set활용

---
+++ 26069.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    unordered_set<string> s;
+    s.insert("ChongChong");
+
+    for (int i = 0; i < N; i++)
+    {
+        string name1;
+        string name2;
+        cin >> name1 >> name2;
+
+        if (s.count(name1) || s.count(name2))   // name1이나 name2가 포함돼있다면
+        {
+            s.insert(name1);
+            s.insert(name2);
+        }
+    }
+
+    cout << s.size() << '\n';
+
+    return 0;
+}
