1c3bb0bd494255a3a2da450c2a67d6f02546387c
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:21:25 +0900

    분수의 합을 기약분수로 구하기

---
+++ 1735.cpp
@@ -0,0 +1,37 @@
+﻿using namespace std;
+#include <iostream>
+
+int Gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+
+    int g = (b * d) / Gcd(b, d);
+
+    int num1 = g / b;
+    b *= num1;
+    a *= num1;
+
+    int num2 = g / d;
+    d *= num2;
+    c *= num2;
+
+    int num3 = a + c;
+    int g2 = Gcd(b, num3);
+
+    b = b / g2;
+    num3 = num3 / g2;
+    cout << num3 << " " << b << '\n';
+}
