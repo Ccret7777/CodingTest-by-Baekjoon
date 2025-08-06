e187d67ce327eca8304d2389fb28b5ca2c6eccbf
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:13:36 +0900

    벡터안의 중복 제거

---
+++ 18870.cpp
@@ -0,0 +1,34 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility> 
+#include <map>
+
+int main()
+{
+    long long N;
+    cin >> N;
+
+    vector<long long> x(N);
+    vector<long long> temp;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x[i];
+        temp.push_back(x[i]);
+    }
+
+    sort(temp.begin(), temp.end());
+    temp.erase(unique(temp.begin(), temp.end()), temp.end());   // 중복 제거
+
+    map<long long, long long> m;
+
+    for (int i = 0; i < temp.size(); i++)
+        m[temp[i]] = i;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << m[x[i]] << " ";
+    }
+}
