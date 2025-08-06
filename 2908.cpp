99fd1fdc74ba4098edb464eda1dae1481c64c916
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:53:34 +0900

    숫자 거꾸로 뒤집기

---
+++ 2908.cpp
@@ -0,0 +1,23 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    string A;
+    string B;
+
+    cin >> A >> B;
+
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+
+    // 문자열을 숫자로
+    int numA = stoi(A);
+    int numB = stoi(B);
+
+    cout << max(numA, numB) << '\n';
+    
+    return 0;
+}
