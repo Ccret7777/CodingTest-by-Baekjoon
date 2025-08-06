b5b06920f2c4257744cfa5a2867d10423a02e320
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:12:16 +0900

    vector의 pair정렬

---
+++ 11650.cpp
@@ -0,0 +1,27 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+int main()
+{
+    long long N;
+    cin >> N;
+
+    vector<pair<long long, long long>> v;
+
+    for (long long i = 0; i < N; i++)
+    {
+        long long x, y;
+        cin >> x >> y;
+        v.push_back({ x, y });
+    }
+
+    sort(v.begin(), v.end());
+
+    for (long long i = 0; i < v.size(); i++)
+    {
+        cout << v[i].first << " " << v[i].second << '\n';
+    }
+}
