68cad17ca4c5081630a2f89daf3f856cc1539334
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:59:03 +0900

    그룹단어 체커/방문 체크

---
+++ 1316.cpp
@@ -0,0 +1,45 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool IsVisited(const string& word)
+{
+    vector<bool> isVisited(26, false);
+
+    char prev = 0;
+
+    for (char c : word)
+    {
+        if (c != prev)
+        {
+            if (isVisited[c - 'a'])
+                return false;
+        }
+
+        isVisited[c - 'a'] = true;
+        prev = c;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    string word;
+    int count = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> word;
+
+        if (IsVisited(word)) count++;
+    }
+
+    cout << count << '\n';
+
+    return 0;
+}
