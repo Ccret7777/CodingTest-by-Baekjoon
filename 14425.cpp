41f0fe74a3fe949ae8c73baeca88e500752aa7c5
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:16:00 +0900

    비교할 때 unorder_set&find함수 이용

---
+++ 14425.cpp
@@ -0,0 +1,35 @@
+﻿using namespace std;
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    unordered_set<string> S;   // 중복없이 저장. map과 달리 키만 저장.
+
+    for (int i = 0; i < N; i++)
+    {
+        string temp;
+        cin >> temp;
+        S.insert(temp);
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < M; i++)
+    {
+        string temp;
+        cin >> temp;
+
+        if (S.find(temp) != S.end())   // temp가 S에 존재?
+            count++;
+    }
+
+    cout << count << '\n';
+}
