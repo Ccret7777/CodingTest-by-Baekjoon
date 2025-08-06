5f68a5557047c013031b9d86a9b81ea9d58c5fdc
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:23:12 +0900

    골드바흐 파티션/짝수를 두 소수의 합으로 나타내기

---
+++ 17103.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 1000000;
+vector<bool> is_prime(MAX + 1, true);
+
+void Sieve() 
+{
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= MAX; i++) 
+    {
+        if (is_prime[i]) 
+        {
+            for (int j = i * i; j <= MAX; j += i) 
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Sieve();
+
+    int T;
+    cin >> T;
+    while (T--) 
+    {
+        int n;
+        cin >> n;
+        int count = 0;
+
+        for (int i = 2; i <= n / 2; i++) 
+        {
+            if (is_prime[i] && is_prime[n - i]) 
+            {
+                count++;
+            }
+        }
+
+        cout << count << '\n';
+    }
+
+    return 0;
+}
