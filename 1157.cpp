6e10465b4367536904ea7affd68bc90479ab3a1a
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:56:24 +0900

    단어에서 가장 많이 등장한 알파벳 개수/vector<pair>이용

---
+++ 1157.cpp
@@ -0,0 +1,40 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    string word;
+    cin >> word;
+    int alpha[26] = { 0 };
+
+    for (int i = 0; i < word.length(); i++)
+    {
+        char ch = tolower(static_cast<unsigned char>(word[i])); // 대소문자 구분 안 하려면
+
+        if ('a' <= ch && ch <= 'z') 
+        {
+            int idx = ch - 'a';
+            alpha[idx]++;
+        }
+    }
+
+    vector<pair<int, char>> alphas;
+
+    for (int i = 0; i < 26; i++)
+    {
+        alphas.push_back({ alpha[i], char('A' + i) });
+    }
+
+    sort(alphas.begin(), alphas.end());   // 먼저 first값 기준으로 비교, 같다면 second값 기준으로 비교
+
+    int max = alphas[25].first;
+    if (max == alphas[24].first)
+        cout << "?" << '\n';
+    else
+        cout << alphas[25].second << '\n';
+
+    return 0;
+}
