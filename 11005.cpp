804a36951f4ea2c9fa88f38c87d0562c28f82131
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:02:54 +0900

    10진법을 B진법으로 변환

---
+++ 11005.cpp
@@ -0,0 +1,27 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    long long N;
+    int B;
+
+    cin >> N >> B;
+
+    string result = "";
+
+    while (N > 0)
+    {
+        int remain = N % B;
+        if (remain < 10)
+            result = char('0' + remain) + result;
+        else
+            result = char('A' + (remain-10)) + result;
+        N /= B;
+    }
+
+    cout << result << '\n';
+
+    return 0;
+}
