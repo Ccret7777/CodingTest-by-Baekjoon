6d1b44d9a2bb54f73272a93466e1c135a46f0a82
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:20:47 +0900

    최대공약수를 이용한 최소공배수 구하기

---
+++ 1934.cpp
@@ -0,0 +1,33 @@
+﻿using namespace std;
+#include <iostream>
+
+using int64 = long long;
+
+// 최대공약수 (GCD) 함수
+int gcd(int a, int b) 
+{
+    while (b != 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--) 
+    {
+        int A, B;
+        cin >> A >> B;
+
+        int g = gcd(A, B);
+        cout << (A * B) / g << '\n'; // 최소공배수 출력
+    }
+
+    return 0;
+}
