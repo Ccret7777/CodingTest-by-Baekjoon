d85aca9ab3235a93c70cee5e9c9afe230bbdd2f9
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:43:17 +0900

    나머지 연산자 활용/자릿수 구하기

---
+++ 2588_20250617.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+
+	int x = b % 10;
+	cout << x * a << endl;
+
+	x = (b / 10) % 10;
+	cout << x * a << endl;
+
+	x = b / 100;
+	cout << x * a  << endl;
+	cout << a * b << endl;
+
+	return 0;
+}
