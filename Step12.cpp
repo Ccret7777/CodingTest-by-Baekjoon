bdbc83637d3b34b450455a4d87940686cfda4d7f
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:10:29 +0900

    블랙잭/3중 for문

---
+++ Step12.cpp
@@ -0,0 +1,38 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N;
+    long long M;
+    vector<int> v;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    int maxSum = 0;
+
+    for (int i = 0; i < N - 2; ++i) 
+    {
+        for (int j = i + 1; j < N - 1; ++j) 
+        {
+            for (int k = j + 1; k < N; ++k)
+            {
+                int sum = v[i] + v[j] + v[k];
+                if (sum <= M && sum > maxSum) 
+                {
+                    maxSum = sum;
+                }
+            }
+        }
+    }
+
+    cout << maxSum << '\n';
+}
