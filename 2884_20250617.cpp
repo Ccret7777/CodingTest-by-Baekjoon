70eef5195a2bd533060594d65c4c668b8b7472e9
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:41:33 +0900

    알람 설정 프로그램(hour, minute 변환)

---
+++ 2884_20250617.cpp
@@ -0,0 +1,20 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	int H, M;
+	cin >> H >> M;
+
+	int result = H * 60;
+	result = result + M - 45;
+
+	if (result < 0) result = result + 24 * 60;
+
+	H = result / 60;
+	M = result % 60;
+
+	cout << H << " " << M << endl;
+
+	return 0;
+}
