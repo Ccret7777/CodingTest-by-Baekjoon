451ef74629d26277f5a752235906cd46fa8ae465
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:24:38 +0900

    Queue기능 구현

---
+++ 18258.cpp
@@ -0,0 +1,63 @@
+﻿using namespace std;
+#include <iostream>
+#include <queue>
+#include <string>
+
+using int64 = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int64 N;
+    cin >> N;
+
+    queue<int64> q;
+
+    while (N--)
+    {
+        string s;
+        cin >> s;
+
+        if (s == "push")
+        {
+            int64 X;
+            cin >> X;
+            q.push(X);
+        }
+
+        else if (s == "pop")
+        {
+            if (q.empty()) cout << -1 << '\n';
+            else
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+
+        else if (s == "size")
+        {
+            cout << q.size() << '\n';
+        }
+
+        else if (s == "empty")
+        {
+            if (q.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
+
+        else if (s == "front")
+        {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+
+        else if (s == "back")
+        {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+}
