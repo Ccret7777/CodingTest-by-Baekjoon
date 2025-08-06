5c45174b249017e948d97476ea1c6f4adf5d806b
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:21:53 +0900

    소수구하기

---
+++ 1929.cpp
@@ -0,0 +1,35 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+using int64 = long long;
+
+bool IsPrime(int64 n)
+{
+    if (n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i++)
+    {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int64 N, M;
+    cin >> N >> M;
+    vector<int64> v;
+
+    for (int64 i = N; i <= M; i++)
+    {
+        if (IsPrime(i))
+            v.push_back(i);
+    }
+
+    for (int64 i = 0; i < v.size(); i++)
+        cout << v[i] << '\n';
+}
