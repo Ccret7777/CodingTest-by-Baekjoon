354370aac5ccc80d5513c867e294bbcb7c7e82e5
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:45:46 +0900

    빠르게 입출력

---
+++ 1552.cpp
@@ -0,0 +1,18 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	ios::sync_with_stdio(false); // C와 C++의 입출력 분리
+	cin.tie(NULL);               // cin과 cout의 연결 끊기
+
+	int T;
+	cin >> T;
+
+	int A, B;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+		cout << "Case #" << i + 1 << ": " << A << " + " << B << " = " << A + B << '\n';
+	}
+}
