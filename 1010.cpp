2aac2dc2547da54afe935951fef208a78ebc338d
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:27:16 +0900

    경우의 수/이항 계수 약분

---
+++ 1010.cpp
@@ -0,0 +1,33 @@
+﻿using namespace std;
+#include <iostream>
+
+using int64 = long long;
+
+int64 Factorial(int n, int r)
+{
+    if (r > n - r) r = n - r;
+
+    int64 result = 1;
+    for (int i = 1; i <= r; ++i)
+    {
+        result *= (n - i + 1);
+        result /= i;
+    }
+    return result;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    int N, M;
+    while (T--)
+    {
+        cin >> N >> M;
+
+        cout << Factorial(M, N) << '\n';
+    }
+
+    return 0;
+}
