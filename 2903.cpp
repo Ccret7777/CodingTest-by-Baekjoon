6e1ac2d57368572d5615d6c394ddf866b3f05716
Eunseo730 <shushu0730@naver.com>
2025-08-07 05:03:56 +0900

    중앙 이동 알고리즘/pow함수

---
+++ 2903.cpp
@@ -0,0 +1,14 @@
+﻿using namespace std;
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int result = pow(2, N) + 1;
+    cout << result * result << '\n';
+
+    return 0;
+}
