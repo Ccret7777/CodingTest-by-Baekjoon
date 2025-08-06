2c08c9c8e8d6b4bca28556348c5c5c01df81526e
Eunseo730 <shushu0730@naver.com>
2025-08-07 04:51:58 +0900

    문장의 단어 개수 세기

---
+++ 1152.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	stringstream ss(s);
+	string word;
+	int count = 0;
+
+	while (ss >> word) count++;    // 공백 기준으로 단어 분리
+
+	cout << count << '\n';
+
+	return 0;
+}
+
+
