995c5db75f78c6ca0aeef974a382f22fdb8b80a8
Ccret <shushu0730@naver.com>
2025-08-09 00:05:03 +0900

    팩토리얼 재귀함수

---
+++ 27433.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+#include <iostream>
+
+using int64 = long long;
+
+int64 Facto(int n)
+{
+    if (n == 0 || n == 1) return 1;
+
+    return n * Facto(n - 1);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    cout << Facto(N) << '\n';
+
+    return 0;
+}
