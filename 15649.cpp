b77dd44f6d037ba340c5f667764c29c63032a571
Ccret <shushu0730@naver.com>
2025-08-09 00:03:34 +0900

    백트래킹/Dfs

---
+++ 15649.cpp
@@ -0,0 +1,41 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int N, M;
+vector<bool> used;
+vector<int> v;
+
+void Dfs(int depth)
+{
+    if (depth == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (!used[i])
+        {
+            used[i] = true;
+            v[depth] = i;
+            Dfs(depth + 1);
+            used[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    used.assign(N + 1, false); // 크기 재설정
+    v.assign(M, 0);
+
+    Dfs(0);
+
+
+    return 0;
+}
