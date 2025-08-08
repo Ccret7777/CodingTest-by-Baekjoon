8c15b799140f02f41eec4425ce534558612b13d8
Ccret <shushu0730@naver.com>
2025-08-09 00:05:33 +0900

    피보나치 수열 재귀함수

---
+++ 10870.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+#include <iostream>
+
+using int64 = long long;
+
+int64 Fibo(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << Fibo(n) << '\n';
+
+    return 0;
+}
