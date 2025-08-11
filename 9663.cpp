bbf1da2d526d4e46ec3bbf5e0e341509fbc036f0
Ccret <shushu0730@naver.com>
2025-08-11 23:44:04 +0900

    백트래킹/체스에서 퀸을 안전한 위치에 놓는 경우의 수

---
+++ 9663.cpp
@@ -0,0 +1,46 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int N;
+vector<int> queens;
+int result = 0;
+
+bool IsSafe(int y, int x)
+{
+    for (int row = 0; row < y; row++)
+    {
+        int c = queens[row];
+        if (c == x) return false;  // 같은 열
+        if (abs(row - y) == abs(x - c)) return false;  // 대각선
+    }
+    return true;
+}
+
+void Dfs(int y)
+{
+    if (y == N)
+    {
+        result++;
+        return;
+    }
+
+    for (int x = 0; x < N; x++)
+    {
+        if (IsSafe(y, x))
+        {
+            queens[y] = x;
+            Dfs(y + 1);
+        }
+    }
+}
+
+int main()
+{
+    cin >> N;
+    queens.resize(N);
+    Dfs(0);
+
+    cout << result << '\n';
+    return 0;
+}
