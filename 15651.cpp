fc02ee05a3b1fc8e60339498a0b9fe2d91974007
Ccret <shushu0730@naver.com>
2025-08-11 23:45:28 +0900

    백트래킹/Dfs3

---
+++ 15651.cpp
@@ -0,0 +1,33 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int N, M;
+vector<int> v;
+
+void Dfs(int depth)
+{
+    if (depth == M)
+    {
+        for (int x : v)
+            cout << x << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        v[depth] = i;
+        Dfs(depth + 1);
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    v.assign(M, 0);
+
+    Dfs(0);
+
+    return 0;
+}
