0be2dea475b413f8231a142d8bdbaf45b90c9cd3
Ccret <shushu0730@naver.com>
2025-08-11 23:45:07 +0900

    백트래킹/Dfs2

---
+++ 15650.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+
+int N, M;
+vector<int> v;
+
+void Dfs(int start)
+{
+    if (v.size() == M)
+    {
+        for (int x : v)
+           cout << x << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i <= N; i++)
+    {
+        v.push_back(i);
+        Dfs(i + 1);   // 다음부턴 i보다 큰 수만 선택
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    Dfs(1);
+
+    return 0;
+}
