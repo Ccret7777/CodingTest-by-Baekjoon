3c2aeaf16f160173c28757fc73c3b3b68440349f
Ccret <shushu0730@naver.com>
2025-08-15 02:03:02 +0900

    백트래킹+조합/start-link최솟값 구하기

---
+++ 14889.cpp
@@ -0,0 +1,62 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+
+int N;
+vector<vector<int>> v;
+vector<bool> selected;
+int result = INT_MAX;
+
+int CalDiff()
+{
+    int startSum = 0;
+    int linkSum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (selected[i] && selected[j])
+                startSum += v[i][j] + v[j][i];
+
+            else if (!selected[i] && !selected[j])
+                linkSum += v[i][j] + v[j][i];
+        }
+    }
+    return abs(startSum - linkSum);
+}
+
+void Dfs(int idx, int count)
+{
+    if (count == N / 2)
+    {
+        result = min(result, CalDiff());
+        return;
+    }
+
+    for (int i = idx; i < N; i++)
+    {
+        selected[i] = true;
+        Dfs(i + 1, count + 1);
+        selected[i] = false;
+    }
+}
+
+int main()
+{
+    cin >> N;
+
+    v.assign(N, vector<int>(N));
+    selected.assign(N, false);
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> v[i][j];
+
+    Dfs(0, 0);
+    cout << result << '\n';
+
+    return 0;
+}
