0b993611c0a1ccafe1b1d58a6697ca16b0037c98
Ccret <shushu0730@naver.com>
2025-08-18 13:48:37 +0900

    동적계획법/파도반 수열

---
+++ 9461.cpp
@@ -0,0 +1,39 @@
+﻿using namespace std;
+#include <iostream>
+
+using int64 = long long;
+
+int64 P(int n)
+{
+    int64 p[110];
+    p[1] = p[2] = p[3] = 1;
+    p[4] = p[5] = 2;
+    p[6] = 3;
+    p[7] = 4;
+    p[8] = 5;
+    p[9] = 7;
+    p[10] = 9;
+
+    for (int i = 11; i <= n; i++)
+    {
+        p[i] = p[i - 2] + p[i - 3];
+    }
+
+    return p[n];
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+
+        cout << P(N) << '\n';
+    }
+
+    return 0;
+}
