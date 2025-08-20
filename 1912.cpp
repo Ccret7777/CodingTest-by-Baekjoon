250c0ecdeeb03fddbe16c4ea350fd6c518cfea07
Ccret <shushu0730@naver.com>
2025-08-20 14:46:46 +0900

    동적계획법/연속합 중에 최댓값

---
+++ 1912.cpp
@@ -0,0 +1,33 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using int64 = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int64 n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    vector<int> dp(n);
+    dp[0] = v[0];
+    int maxSum = v[0];
+
+    for (int i = 1; i < n; i++) 
+    {
+        dp[i] = max(v[i], dp[i - 1] + v[i]); // 이어가기 vs 새로 시작
+        maxSum = max(maxSum, dp[i]);         // 최대값 갱신
+    }
+
+    cout << maxSum << '\n';
+
+    return 0;
+}
