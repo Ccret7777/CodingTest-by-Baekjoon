756a7ad3c45f365d074c739ca332b35a8a3d8bb3
Ccret <shushu0730@naver.com>
2025-08-21 09:35:24 +0900

    동적계획법/삼각형 합 최대경로

---
+++ 1932.cpp
@@ -0,0 +1,40 @@
+﻿using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> trangle(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cin >> trangle[i][j];
+        }
+    }
+
+    // DP 진행 (위에서부터 내려오기)
+    for (int i = 1; i < n; i++) // 두 번째 줄부터 시작
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0) // 왼쪽 끝
+                trangle[i][j] += trangle[i - 1][j];
+            else if (j == i) // 오른쪽 끝
+                trangle[i][j] += trangle[i - 1][j - 1];
+            else // 가운데
+                trangle[i][j] += max(trangle[i - 1][j - 1], trangle[i - 1][j]);
+        }
+    }
+
+    // 마지막 줄에서 최대값 찾기
+    int result = *max_element(trangle[n - 1].begin(), trangle[n - 1].begin() + n);
+    cout << result << "\n";
+
+    return 0;
+}
